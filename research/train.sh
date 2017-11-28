814de4eb5ae61340428e675c0bf587ab99ad55c9
shashank <rajput.shashank11@gmail.com>
2017-11-28 13:45:27 -0600

    Validated.

---
+++ research/train.sh
@@ -0,0 +1,3 @@
+python object_detection/train.py \
+    --pipeline_config_path="/home/shashank/Workdir/models/research/object_detection/samples/configs/ssd_inception_v2_coco.config" \
+    --train_dir="/home/shashank/nmt_models/object_detection/"
